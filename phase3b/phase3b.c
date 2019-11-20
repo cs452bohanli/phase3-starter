d022b5df84ed8626e641eaedefb12f029b371a5f
bohan li <li.3.141592653589@gmail.com>
2019-11-19 22:06:37 -0700

    finished implementation

--- phase3b/phase3b.c
+++ phase3b/phase3b.c
@@ -30,13 +30,36 @@ P3PageFaultHandler(int type, void *arg)
         print error message
         USLOSS_Halt(1)
     *********************/
-
+	int rc;
+	if (USLOSS_MmuGetCause() == USLOSS_MMU_FAULT) {
+		USLOSS_PTE *table;
+		rc = P3PageTableGet(P1_GetPid(), &table);
+		if (rc != P1_SUCCESS || table == NULL) {
+			USLOSS_Console("Error: process has no page table\n");
+			USLOSS_Halt(1);
+		}
+		else {
+			int page = ((int) arg) / USLOSS_MmuPageSize();
+			table[page].incore = table[page].read = table[page].write = 1;
+			table[page].frame = page;
+			assert(USLOSS_MmuSetPageTable(table) == USLOSS_MMU_OK);
+		}
+	}
+	else {
+		USLOSS_Console("Error: page fault not caused my mmu fault\n");
+		USLOSS_Halt(1);
+	}
 }
 
 USLOSS_PTE *
 P3PageTableAllocateEmpty(int pages)
 {
-    USLOSS_PTE  *table = NULL;
+    USLOSS_PTE  *table = (USLOSS_PTE*) malloc(pages*sizeof(USLOSS_PTE));
+	if (table) {
+		for (int i = 0; i < pages; i++){
+			table[i].incore = 0;
+		}
+	}
     // allocate and initialize an empty page table
     return table;
 }
